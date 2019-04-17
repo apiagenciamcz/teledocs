eda69d05362489466c3c5b40107d3c4486f7d161
APIDigitalMCZ <api.agenciadigital@gmail.com>
2019-04-16 22:39:42 -0300

    UpDate Visita06

--- src/pages/Visitas/Visita06.js
+++ src/pages/Visitas/Visita06.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet} from 'react-native';
-import { Form, Content, Left, Body, Text, Button, Item, Footer, Right, Card, CardItem, CheckBox, View} from 'native-base';
+import { ScrollView, StyleSheet, Image} from 'react-native';
+import { Header, Left, Body, Text, Form, Title, Drawer, Button, Right, Item, Card, CardItem, View, Label, Input, List, ListItem} from 'native-base';
 
-import Voltar from 'react-native-vector-icons/EvilIcons';
-import IcoHome from 'react-native-vector-icons/MaterialCommunityIcons';
+import Voltar from 'react-native-vector-icons/FontAwesome';
+import { RadioButton, Rad } from 'react-native-paper';
 import {Actions} from 'react-native-router-flux';
-
+import MenuHeader from 'react-native-vector-icons/AntDesign';
 import Modal from "react-native-modal";
 import { TextInputMask } from 'react-native-masked-text';
 
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import SideBar from '../SideBar';
+
 export default class Visita06 extends Component{
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-          cartao: '',
-          nascimento: '',
-          verificador: '',
-        }
-      }
+  state = {
+    checked: 'master',
+  };
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cartao: '',
+      nascimento: '',
+      verificador: '',
+    }
+  }
 
     home(){
         Actions.home()
@@ -32,6 +40,14 @@ export default class Visita06 extends Component{
         Actions.visita07()
     };
 
+    delmeiosdepagt(){
+      Actions.delmeiosdepagt()
+  };
+
+  addmeiosdepagt(){
+    Actions.addmeiosdepagt()
+};
+
     state = {
         isModalVisible: false,
       };
@@ -41,47 +57,46 @@ export default class Visita06 extends Component{
 
     render(){
         return(
+          <Drawer ref={(ref) => { this._drawer = ref; }} 
+          content={<SideBar navigator={this._navigator} />} 
+          onClose={() => this.closeDrawer()} >
+          <Header style={{backgroundColor:'#282c34', shadowColor:'blue', shadowOpacity:2, height:60}}>
+              <Item style={{borderBottomWidth:0, marginLeft:-290}}>
+                  <MenuHeader name="arrowleft" size={30} color="#fff" onPress={this.visita04} />
+              </Item> 
+          </Header>
 
-            <ScrollView style={{backgroundColor:'#eaebed'}}>
-
-
-
-            
-            <Content style={{padding:20}}>
-                <Item style={{borderBottomWidth:0}}>
+            <ScrollView style={{backgroundColor:'#eaebed', padding:20}} scrollEnabled={true}>
+            <Item style={{borderBottomWidth:0}}>
                     <Text style={{color:"#282c34", fontSize:22, fontWeight:"bold", marginBottom:20}}>Por favor, confirme o seu método de pagamento</Text>
                 </Item>
-                
-                <Card>
-            <CardItem header>
-              <Text>DADOS DO CARTÃO</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text style={{fontWeight:'500'}}>BANDEIRA: <Text style={{fontWeight:'100'}}>MasterCard</Text></Text>
-                <Text style={{fontWeight:'500'}}>TITULAR: <Text style={{fontWeight:'100'}}>Israel Moreira</Text></Text>
-                <Text style={{fontWeight:'500'}}>CARTÃO: <Text style={{fontWeight:'100'}}>4001 6358 0635 0157</Text></Text>
-                <Text style={{fontWeight:'500'}}>VALIDADE: <Text style={{fontWeight:'100'}}>05/19</Text></Text>
-                <Text style={{fontWeight:'500'}}>CÓDIGO: <Text style={{fontWeight:'100'}}>579</Text></Text>
-              </Body>
-            </CardItem>
-         </Card>
-                    
-         <Button transparent block onPress={this._toggleModal}>
-            <Text style={{color:'#282c34'}}>Adicionar Novo Cartão</Text>
-          </Button>
-                <Form>
-                    
-                    <Button style={{padding:10, backgroundColor:'#17a2b8', width:'100%', marginTop:20}} onPress={this._toggleModal}>
-                        <Text style={{color:'white', flex:1, textAlign:'center'}} onPress={this.visita07}>AVANÇAR</Text>
-                    </Button>
-                </Form>
 
+                <RadioButton.Group
+        onValueChange={value => this.setState({ value })}
+        value={this.state.value}
+      >
+                           
+            
+                <List style={{backgroundColor:'#fff', marginTop:20}}>
+                <ListItem>
+                  <Left>
+                  <RadioButton value="master" color="#282c34"/><Image source={require('../../images/mastercard.jpg')} style={{marginLeft:10}} /><Text style={{marginLeft:10}}>**** 4182</Text>
+                  </Left>
+                  <Right>
+                  </Right>
+                </ListItem>
+                <ListItem >
+                <Left>
+                <RadioButton value="visa" color="#282c34"/><Image source={require('../../images/visa.jpg')} style={{marginLeft:10}}/><Text style={{marginLeft:10}}>**** 0695</Text>
+                  </Left>
+                  <Right>
+                  </Right>
+                </ListItem>
+              </List>
+              </RadioButton.Group>
 
-                
-
-        
-            </Content>
+                <Button transparent disabled style={{padding:10, width:'100%', marginTop:20}} onPress={this.addmeiosdepagt}><Text style={{color:'#282c34', flex:1, textAlign:'center'}}>ADICIONAR NOVO CARTÃO</Text></Button>
+                <Button style={{padding:10, backgroundColor:'#17a2b8', width:'100%', marginTop:20}} onPress={this.visita07}><Text style={{color:'white', flex:1, textAlign:'center'}}>AVANÇAR</Text></Button>
 
 
             
@@ -147,34 +162,37 @@ export default class Visita06 extends Component{
           </View>
         </Modal>
             </ScrollView>
+        </Drawer>
         )
     }
 }
 
 const styles = StyleSheet.create({
   title: {
-    flex: 1,
-    color:'#282c34',
-    fontSize:18,
-    fontWeight:'500',
-    marginBottom:10,
-},
-label:{
-    fontSize: 14,
-    color:'#999',
-    padding:3,
+      flex: 1,
+      color:'#282c34',
+      fontSize:18,
+      fontWeight:'500',
+      marginBottom:10,
+  },
+  label:{
+      fontSize: 14,
+      color:'#999',
+      padding:3,
+      flex:1,
+      marginTop:-10,
+  },
+  inputMask:{
+    width: '100%',
+    borderColor: '#999',
+    borderBottomWidth: 1,
+    marginBottom:40,
+    marginLeft:-10,
     flex:1,
-    marginTop:-10,
-},
-inputMask:{
-  width: '100%',
-  borderColor: '#999',
-  borderBottomWidth: 1,
-  marginBottom:40,
-  marginLeft:-10,
-  flex:1,
-  alignContent:'center',
-  color: '#282c34'
-},
+    alignContent:'center',
+    color: '#282c34'
+  },
 
-  });
+
+
+});
